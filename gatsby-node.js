abe68f65ebe3013a9b7aed21bc7fb091436afa5b
Orange <jtcanton@gmail.com>
2022-06-09 14:46:32 -0700

    hid graphql query in gatsby node . js

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,43 +6,40 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   // Define a template for blog post
   const blogPost = path.resolve('./src/templates/blog-post.js')
 
-  const result = await graphql(
-    `
-      {
-        
-      }
-    `
-  )
+  // const result = await graphql(
+  //   `
+  //   `
+  // )
 
-  if (result.errors) {
-    reporter.panicOnBuild(
-      `There was an error loading your Contentful posts`,
-      result.errors
-    )
-    return
-  }
+  // if (result.errors) {
+  //   reporter.panicOnBuild(
+  //     `There was an error loading your Contentful posts`,
+  //     result.errors
+  //   )
+  //   return
+  // }
 
-  const posts = result.data.allContentfulBlogPost.nodes
+  //const posts = result.data.allContentfulBlogPost.nodes
 
   // Create blog posts pages
   // But only if there's at least one blog post found in Contentful
   // `context` is available in the template as a prop and as a variable in GraphQL
 
-  if (posts.length > 0) {
-    posts.forEach((post, index) => {
-      const previousPostSlug = index === 0 ? null : posts[index - 1].slug
-      const nextPostSlug =
-        index === posts.length - 1 ? null : posts[index + 1].slug
+  // if (posts.length > 0) {
+  //   posts.forEach((post, index) => {
+  //     const previousPostSlug = index === 0 ? null : posts[index - 1].slug
+  //     const nextPostSlug =
+  //       index === posts.length - 1 ? null : posts[index + 1].slug
 
-      createPage({
-        path: `/blog/${post.slug}/`,
-        component: blogPost,
-        context: {
-          slug: post.slug,
-          previousPostSlug,
-          nextPostSlug,
-        },
-      })
-    })
-  }
+  //     createPage({
+  //       path: `/blog/${post.slug}/`,
+  //       component: blogPost,
+  //       context: {
+  //         slug: post.slug,
+  //         previousPostSlug,
+  //         nextPostSlug,
+  //       },
+  //     })
+  //   })
+  // }
 }
